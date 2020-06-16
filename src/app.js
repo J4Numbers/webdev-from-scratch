3102cfa0b0a240098be240d3bde9efcdd773ba33
Jayne Doe <j4numbers@gmail.com>
2020-06-16 20:33:36 +0100

    feat(app): barebones creation of an application

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const config = require('config');
+const restify = require('restify');
+
+const package = require('../package');
+
+const server = restify.createServer({
+  name: config.get('app.name'),
+  url: config.get('app.hostname'),
+  version: package.version,
+  ignoreTrailingSlash: true,
+});
+
+server.get('/', (req, res, next) => {
+  console.log(`${req.getPath()} was called`);
+  res.contentType = 'text/plain';
+  res.send(200, 'Hello world');
+  next();
+});
+
+server.listen(config.get('app.port'), () => {
+  console.log(`${server.name} has started at location ${server.url}`);
+});
