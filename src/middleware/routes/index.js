eb4369ec8e2d71aad07b734cc5a6567d32a9a821
Jayne Doe <j4numbers@gmail.com>
2020-06-23 20:33:04 +0100

    feat(assets): added routes into an asset folder

--- src/middleware/routes/index.js
+++ src/middleware/routes/index.js
@@ -1,6 +1,8 @@
+const assetRoutes = require('./assets');
 const homepageRoutes = require('./homepage');
 
 module.exports = (server) => {
+  assetRoutes(server);
   homepageRoutes(server);
   return server;
 };
