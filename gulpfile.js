a198b04ca789fd402d27a3d9c2ffec688fb2120c
Jayne Doe <j4numbers@gmail.com>
2020-06-23 21:08:26 +0100

    build(gulpfile): added gulp as a build engine for scss

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+/* -------------------------------------------------------------- GLOBALS */
+
+const babelDir = './src/javascript/**/*.js';
+const fontDir = './src/fonts/**/*.{woff,woff2}';
+const imageDir = './src/images/**/*.{png,ico,gif,jpg,svg,xml}';
+const sassDir = './src/stylesheets/**/*.scss';
+const bootstrapJsFiles = './node_modules/bootstrap/js/dist/**/*.js';
+
+const destDir = './public/';
+const destBabelDir = `${destDir}javascript`;
+const destFontDir = `${destDir}fonts`;
+const destImageDir = `${destDir}images`;
+const destSassDir = `${destDir}stylesheets`;
+
+/* ---------------------------------------------------------------- CLEAN */
+
+gulp.task('clean', () => gulp
+  .src([destDir], { read: false, allowEmpty: true })
+  .pipe(clean()));
+
+/* --------------------------------------------------------------- ASSETS */
+
+gulp.task('sass', () => gulp
+  .src([sassDir])
+  .pipe(sass({
+    errLogToConsole: true,
+    outputStyle: 'compressed',
+    indentedSyntax: false,
+  }).on('error', sass.logError))
+  .pipe(gulp.dest(destSassDir)));
+
+gulp.task('babel', () => gulp
+  .src([babelDir])
+  .pipe(babel({
+    presets: ['@babel/env'],
+  }))
+  .pipe(gulp.dest(destBabelDir)));
+
+gulp.task('copy-fonts', () => gulp
+  .src([fontDir])
+  .pipe(gulp.dest(destFontDir)));
+
+gulp.task('copy-images', () => gulp
+  .src([imageDir])
+  .pipe(gulp.dest(destImageDir)));
+
+gulp.task('copy-scripts', () => gulp
+  .src([bootstrapJsFiles])
+  .pipe(gulp.dest(destBabelDir)));
+
+/* ---------------------------------------------------------- BUILD TASKS */
+
+gulp.task('build', gulp.series(
+  'clean',
+  gulp.parallel('sass', 'babel'),
+  gulp.parallel('copy-fonts', 'copy-images', 'copy-scripts'),
+));
+
+gulp.task('default', gulp.series('build'));
